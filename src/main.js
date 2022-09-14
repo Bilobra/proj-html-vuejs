f5f76d3227b24dcf5011eb9b4ca0ed079311e134
Bilobra <bra.carrozzo@gmail.com>
2022-09-14 12:49:36 +0200

    import font awesome

--- src/main.js
+++ src/main.js
@@ -13,6 +13,29 @@ Vue.use(BootstrapVue)
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin)
 
+// FONTAWESOME
+
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
+import { faFacebookF} from '@fortawesome/free-brands-svg-icons'
+import { faTwitter} from '@fortawesome/free-brands-svg-icons'
+import { faInstagram} from '@fortawesome/free-brands-svg-icons'
+import { faGithub} from '@fortawesome/free-brands-svg-icons'
+
+
+/* add icons to the library */
+library.add(faMagnifyingGlass,faCartShopping,faFacebookF,faTwitter,faInstagram,faGithub)
+
+/* add font awesome icon component */
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 Vue.config.productionTip = false
 
 new Vue({
