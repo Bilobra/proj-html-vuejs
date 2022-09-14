381a84175c9780452a90a0dfbed45f856c6284f4
Bilobra <bra.carrozzo@gmail.com>
2022-09-14 17:00:05 +0200

    Add Hero Section

---
+++ src/components/HeroSection.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="row row-cols-1">
+        <div class="col-6 d-flex flex-column gap-4 col_info">
+            <h2 class="hero_title">
+                The biggest platform for your growth.
+            </h2>
+            <p class="hero_description">
+                There are many variations of passages of Lorem Ipsum avaiable, but the majority have suffred alteration
+                in some form
+            </p>
+            <div class="hero_button">
+                <input type="button" value="Read More" class="button_default">
+
+            </div>
+        </div>
+        <div class="col-6 ">
+            <img src="../assets/img/27-1.png" alt="hero-img peopletalking" class="img_h">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HeroSection',
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../style/VarColor.scss';
+
+.col_info {
+    padding-top: 80px;
+    padding-right: 110px;
+}
+
+.img_h {
+    height: 510px;
+    position: relative;
+    bottom: -40px;
+}
+
+.hero_title {
+    font-size: 34px;
+}
+
+.hero_description {
+    font-size: 16px;
+    color: $small-text-color;
+    
+}
+</style>
---
+++ src/components/MainComponent.vue
@@ -0,0 +1,32 @@
+<template>
+    <main>
+        <section id="hero_section">
+            .<div class="container_hero_section">
+                <HeroSection />
+
+            </div>
+        </section>
+    </main>
+</template>
+
+<script>
+import HeroSection from './HeroSection.vue'
+export default {
+    name: 'MainComponent',
+    components: {
+        HeroSection,
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    @import '../style/VarColor.scss';
+    #hero_section{
+        background-color: $jumbo-bg-color;
+
+        .container_hero_section{
+            max-width: 1441px;
+            margin: 0 auto;
+        }
+    }
+</style>
